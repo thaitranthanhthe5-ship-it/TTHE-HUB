9efbf373c5be2e307108e4400d1ca6f09f60532f
TTHE-HUB <thaitranthanhthe5@gmail.com>
2026-04-21 16:48:35 +0700

    Add TTHE HUB initialization and notification logic
    Initialize configuration and load scripts based on universe ID. Notify user about the loading status and game support.

---
+++ TTHE_HUB.lua
@@ -0,0 +1,55 @@
+_G.ThemeID = _G.ThemeID or nil
+_G.AutoTranslate = (_G.AutoTranslate == nil) and true or _G.AutoTranslate
+_G.SaveConfig = (_G.SaveConfig == nil) and true or _G.SaveConfig
+
+local HttpService = game:GetService("HttpService")
+local StarterGui = game:GetService("StarterGui")
+local placeId = game.PlaceId
+
+local UniverseID = HttpService:JSONDecode(
+    game:HttpGet("https://apis.roblox.com/universes/v1/places/"..placeId.."/universe")
+).universeId
+
+if UniverseID == 994732206 then
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/thaitranthanhthe5-ship-it/TTHE-HUB/refs/heads/main/TTHE-HUB.lua"))()
+
+    task.wait(1)
+
+    StarterGui:SetCore("SendNotification",{
+        Title = "TTHE HUB",
+        Text = "Nà Ná Na Na Ná",
+        Duration = 3
+    })
+
+    task.wait(3)
+
+    StarterGui:SetCore("SendNotification",{
+        Title = "TTHE HUB",
+        Text = "Anh Thế Lỏ",
+        Duration = 2
+    })
+
+    task.wait(2)
+
+    StarterGui:SetCore("SendNotification",{
+        Title = "TTHE HUB",
+        Text = "Loader Success!",
+        Duration = 5
+    })
+
+    task.wait(1)
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Graihub/Gay/refs/heads/main/bamia"))()
+
+else
+    StarterGui:SetCore("SendNotification",{
+        Title = "TTHE HUB",
+        Text = "Game Not Supported",
+        Duration = 5
+    })
+
+    task.wait(1)
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Graihub/notsupport/refs/heads/main/Ui"))()
+end
