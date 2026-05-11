3a91b7a081f52574b3e03f6f4baa1fcb393f326b
TTHE-HUB <thaitranthanhthe5@gmail.com>
2026-05-11 14:54:50 +0700

    Update TTHE_HUB.lua

--- TTHE_HUB.lua
+++ TTHE_HUB.lua
@@ -25,18 +25,3 @@ getgenv().config =  {
     autoV4 = true, 
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/Bountynew.lua"))()
-getgenv().Config = {
-    Team = "Pirates",
-    HideUI = true,
-    HuntConfig = {
-        ["Earned Notification Enabled"] = true,
-        ["Reset Farm (New)"] = true,
-        ["Chat"] = false,
-        ["Farm Delay"] = 0.22, -- 0.15 - Super Fast(Risk - Kick) | 0.22 Fast | 0.35 Medium | Max 0.5
-        ["Webhook"] = {
-            Enabled = false,
-            Url = ""
-        }
-    }
-}
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/2ffcdb62773f587bfb9eb0d52bb35b0c.lua"))()
