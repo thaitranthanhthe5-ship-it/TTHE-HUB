84e48f28e14bc80c17fe20b18ec8500f07cc1eb2
TTHE-HUB <thaitranthanhthe5@gmail.com>
2026-05-13 22:20:28 +0700

    Update mainbanana.lua

--- mainbanana.lua
+++ mainbanana.lua
@@ -1,6 +1,6 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
 getgenv().Key = "7a4b659bc7d29d699ebbeeed" 
 getgenv().config =  {
-    attackSpeed = 0.000000000000000000001,
+    attackSpeed = 0.0000000000000000000000000000000001,
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))()
