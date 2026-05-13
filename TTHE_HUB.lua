5eb59ddfc743103baadeca96620c30cc60bed9ab
TTHE-HUB <thaitranthanhthe5@gmail.com>
2026-05-13 21:16:26 +0700

    Update TTHE_HUB.lua

--- TTHE_HUB.lua
+++ TTHE_HUB.lua
@@ -29,7 +29,7 @@ end)
         end)
     end
 end)
-
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 getgenv().Key = "7a4b659bc7d29d699ebbeeed"
 getgenv().config =  {
     team = "Pirates", -- "Pirates" "Marines"
