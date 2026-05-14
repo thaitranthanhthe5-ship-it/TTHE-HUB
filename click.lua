464d189b6a93f403a5c512d8bbf7857289c7f12d
TTHE-HUB <thaitranthanhthe5@gmail.com>
2026-05-14 17:46:18 +0700

    Update click.lua

--- click.lua
+++ click.lua
@@ -3,5 +3,5 @@ local VirtualUser = game:GetService("VirtualUser")
 while true do
     VirtualUser:CaptureController()
     VirtualUser:ClickButton1(Vector2.new(9999,9999))
-    task.wait(0.000001) -- tốc độ click
+    task.wait(0.00000000000000001) -- tốc độ click
 end
